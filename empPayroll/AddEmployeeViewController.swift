b7fa1eea698e880d3a10cf8e9f28df9d9971d3bb
MacStudent <macstudent@20X-iMacXX-M-419.local>
2018-08-20 17:36:06 -0400

    Final commit

--- empPayroll/AddEmployeeViewController.swift
+++ empPayroll/AddEmployeeViewController.swift
@@ -61,18 +61,90 @@ class AddEmployeeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
     @IBAction func btnCreate(_ sender: UIButton) {
         
         let empId = txtEmpId.text
         let empName = txtEmpName.text
         let empAge = txtEmpAge.text
-        let <#name#> = <#value#>
         
-        
-        
+    
         
     }
     
+    @IBAction func segVehicle(_ sender: UISegmentedControl) {
+        if segVehicle.selectedSegmentIndex == 0 {
+            
+            if segVehicleType.selectedSegmentIndex == 0 {
+                
+                print("Motorcycle")
+                txtMColor.isHidden = false
+                txtMRegPlate.isHidden = false
+                txtMmake.isHidden = false
+                txtMinsurance.isHidden = false
+                viewCar.isHidden = true
+             
+                
+            } else if segVehicleType.selectedSegmentIndex == 1 {
+                
+               txtCcolor.isHidden = false
+                txtCinsurance.isHidden = false
+                txtCmaker.isHidden = false
+                txtCregNo.isHidden = false
+                txtCtransmission.isHidden = false
+                viewCar.isHidden = false
+                print("Car")
+            }
+        
+        }else
+    {
+        viewCar.isHidden = true
+    print("Employee has no vehicle")
+    }
+        
+    }
+    
+    @IBAction func segEmpType(_ sender: UISegmentedControl) {
+        if segEmpType.selectedSegmentIndex == 0 {
+            viewFullTime.isHidden = false
+            txtFTsalary.isHidden = false
+            txtFTbonus.isHidden = false
+            viewIntern.isHidden = true
+            viewCommission.isHidden = true
+            viewFixed.isHidden = true
+            print("Emp is Full Time")
+            
+        }else if segEmpType.selectedSegmentIndex == 1 {
+            viewIntern.isHidden = false
+            txtInternSchool.isHidden = false
+            txtInternStipened.isHidden = false
+            viewFullTime.isHidden = true
+            viewCommission.isHidden = true
+            viewFixed.isHidden = true
+            print("Emp is Intern")
+            
+        }else if segEmpType.selectedSegmentIndex == 2 {
+            viewCommission.isHidden = false
+            txtComRate.isHidden = false
+            txtComPercent.isHidden = false
+            txtComHrsWorked.isHidden = false
+            viewFullTime.isHidden = true
+            viewIntern.isHidden = true
+            viewFixed.isHidden = true
+            print("Emp is Comission Based Part Time")
+            
+        }else if segEmpType.selectedSegmentIndex == 3 {
+            viewFixed.isHidden = false
+            txtFixedAmt.isHidden = false
+            txtFixedRate.isHidden = false
+            txtFixedHrsWorked.isHidden = false
+            viewFullTime.isHidden = true
+            viewIntern.isHidden = true
+            viewCommission.isHidden = true
+            print("Emp is Fixed Based Part Time")
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
